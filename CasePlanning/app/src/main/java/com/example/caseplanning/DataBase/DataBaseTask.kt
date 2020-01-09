db8f5eda4182d571d27829664593129d01ac25f2
Julia <negodyaeva.yulya@mail.ru>
2020-01-10 00:41:08 +0800

    add rea data in database

--- CasePlanning/app/src/main/java/com/example/caseplanning/DataBase/DataBaseTask.kt
+++ CasePlanning/app/src/main/java/com/example/caseplanning/DataBase/DataBaseTask.kt
@@ -1,26 +1,44 @@
 package com.example.caseplanning.DataBase
 
+
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
-class DataBaseTask {
+class DataBaseTask() {
 
     private lateinit var mAuth : FirebaseAuth
     private lateinit var dataBaseReference : DatabaseReference
+    private lateinit var textTask:String
+    private lateinit var listTask : ArrayList<String>
+    private lateinit var listTaskView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+
+    constructor(listTaskView: ListView, textTask:String, adapter: ArrayAdapter<String>) : this() {
+        this.listTaskView = listTaskView
+        this.textTask = textTask
+        this.adapter = adapter
+    }
 
 
+    fun createDataBase(){
+
+        readDataBase()
+
+    }
     fun writeDataBase(listTask: ListView){
         val dataBase = FirebaseDatabase.getInstance()
         dataBaseReference = dataBase.reference
+        dataBaseReference.setValue(textTask)
 
-        dataBaseReference.setValue(listTask)
     }
 
     fun readDataBase(){
 
-        dataBaseReference.addValueEventListener(object: ValueEventListener{
+        val user = mAuth.currentUser
+        dataBaseReference.child(user!!.uid).addValueEventListener(object: ValueEventListener{
             override fun onCancelled(dataBaseError: DatabaseError) {
 
                 Log.d("Tag","Failed to read value", dataBaseError.toException())
@@ -28,11 +46,21 @@ class DataBaseTask {
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
+                val listTaskGenerate:GenericTypeIndicator<ArrayList<String>> = object :
+                    GenericTypeIndicator<ArrayList<String>>() {}
+                listTask = dataSnapshot.child("Tasks").getValue(listTaskGenerate)!!
+                updateUI(adapter,listTaskView)
                 val value = dataSnapshot.getValue(String::class.java)
                 Log.d("Tag", "Value is: $value ")
 
             }
 
         })
+    }
+
+    private fun updateUI(adapter: ArrayAdapter<String>, listTaskView: ListView) {
+
+        listTaskView.adapter = adapter
+
     }
 }
