d1ba1974bc873d22fdcbbbe09d7e87b7231a6acc
Julia <negodyaeva.yulya@mail.ru>
2020-01-19 20:05:42 +0800

    add class Task

---
+++ CasePlanning/app/src/main/java/com/example/caseplanning/DataBase/Task.kt
@@ -0,0 +1,18 @@
+package com.example.caseplanning.DataBase
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Task (
+    var id: String ? = "",
+    var name: String,
+    var photo: String ? = "",
+    var audio: String ? = "",
+    var video: String ? = "",
+    var text: String ? = "",
+    var color: String ? = "",
+    var day: String ? = "",
+    var timeStart: String ? = "",
+    var timeEnd: String ? = "",
+    var shouldRepeat: Boolean
+)
