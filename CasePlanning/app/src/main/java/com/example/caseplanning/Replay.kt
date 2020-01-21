45c553d31b341d32838d76ac1b383f8a3e4cc095
Julia <negodyaeva.yulya@mail.ru>
2020-01-21 18:03:15 +0800

    add list replay

---
+++ CasePlanning/app/src/main/java/com/example/caseplanning/Replay.kt
@@ -0,0 +1,75 @@
+package com.example.caseplanning
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import butterknife.ButterKnife
+import butterknife.OnClick
+
+class Replay : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val viewFragment = inflater.inflate(R.layout.replay, container, false)
+
+        val toolbar = viewFragment.findViewById<Toolbar>(R.id.toolbar)
+        val activity = activity as AppCompatActivity?
+        activity!!.setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = activity.supportActionBar
+        actionBar!!.title = "Период"
+
+        ButterKnife.bind(this, viewFragment)
+
+        listReplay(viewFragment)
+
+        return viewFragment
+    }
+
+    fun listReplay(view: View) {
+        val listReplay = view.findViewById<ListView>(R.id.listViewReplay)
+        val listReplayString = arrayListOf(
+            "Понедельник",
+            "Вторник",
+            "Среда",
+            "Четверг",
+            "Пятница",
+            "Суббота",
+            "Воскресенье"
+        )
+
+        val adapter = ArrayAdapter<String>(
+            activity!!.applicationContext,
+            android.R.layout.simple_list_item_1,
+            listReplayString
+        )
+
+        listReplay.adapter = adapter
+
+        listReplay.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val textView = view as TextView
+                val period = textView.text.toString()
+                val createTaskWindow = CreateTaskWindow()
+                val args = Bundle()
+                args.putString("Period", period)
+                createTaskWindow.arguments = args
+                fragmentManager!!.beginTransaction().replace(R.id.linerLayout,
+                    createTaskWindow).commit()
+
+
+
+
+            }
+    }
+}
