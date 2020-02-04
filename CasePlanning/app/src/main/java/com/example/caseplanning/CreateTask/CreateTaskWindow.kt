2609009f51395c807bf656ba3492f619a4238773
Julia <negodyaeva.yulya@mail.ru>
2020-02-04 18:24:50 +0800

    add dynamic layout sub tasks

--- CasePlanning/app/src/main/java/com/example/caseplanning/CreateTask/CreateTaskWindow.kt
+++ CasePlanning/app/src/main/java/com/example/caseplanning/CreateTask/CreateTaskWindow.kt
@@ -1,28 +1,20 @@
 package com.example.caseplanning.CreateTask
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewStub
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import butterknife.ButterKnife
 import butterknife.OnClick
-import com.example.caseplanning.DataBase.DataBaseTask
-import com.example.caseplanning.Increase.PhotoIncrease
-import com.example.caseplanning.MainWindowCasePlanning
 import com.example.caseplanning.R
-import com.example.caseplanning.TypeTask.*
-import kotlinx.android.synthetic.main.task_window.*
 
 class CreateTaskWindow : Fragment() {
 
@@ -77,7 +69,7 @@ class CreateTaskWindow : Fragment() {
         if (savedInstanceState != null) {
 
             textTask = savedInstanceState.getString("nameTask", "")
-          //  inizializationEdit()
+            //  inizializationEdit()
 
         } else {
 
@@ -112,139 +104,178 @@ val textView = TextView(activity!!.applicationContext)
     ll.addView(textView, lp)
  */
     /*добавление фото задачи*/
-  /*  @OnClick(R.id.photo)
-    fun onClickAddPhoto() {
+    /*  @OnClick(R.id.photo)
+      fun onClickAddPhoto() {
 
-        val photo: Fragment = Photo()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val photo: Fragment = Photo()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.add(R.id.typeTask, photo)
-        transaction.commit()
+          transaction.add(R.id.typeTask, photo)
+          transaction.commit()
 
-    }
+      }
 
-    /*добавление видео задачи*/
-    @OnClick(R.id.video)
-    fun onClickAddVideo() {
+      /*добавление видео задачи*/
+      @OnClick(R.id.video)
+      fun onClickAddVideo() {
 
-        val video: Fragment = Video()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val video: Fragment = Video()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.add(R.id.typeTask, video)
-        transaction.commit()
+          transaction.add(R.id.typeTask, video)
+          transaction.commit()
 
-    }
+      }
 
-    /*добавление аудио задачи*/
-    @OnClick(R.id.audio)
-    fun onClickAddAudio() {
+      /*добавление аудио задачи*/
+      @OnClick(R.id.audio)
+      fun onClickAddAudio() {
 
-        val audio = AudioTask()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val audio = AudioTask()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.add(R.id.typeTask, audio)
-        transaction.commit()
+          transaction.add(R.id.typeTask, audio)
+          transaction.commit()
 
-    }
+      }
 
-    /*добавление задачи в виде списка*/
-    @OnClick(R.id.to_do)
-    fun onClickAddToDoTask() {
+      /*добавление задачи в виде списка*/
+      @OnClick(R.id.to_do)
+      fun onClickAddToDoTask() {
 
-        val toDoTask = ToDoTask()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val toDoTask = ToDoTask()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.add(R.id.typeTask, toDoTask)
-        transaction.commit()
+          transaction.add(R.id.typeTask, toDoTask)
+          transaction.commit()
 
-    }
+      }
 
-    /*добавление задачи в виде текста*/
-    @OnClick(R.id.text)
-    fun onClickAddTextTask() {
+      /*добавление задачи в виде текста*/
+      @OnClick(R.id.text)
+      fun onClickAddTextTask() {
 
-        val textTask = TextTask()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val textTask = TextTask()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.add(R.id.typeTask, textTask)
-        transaction.commit()
+          transaction.add(R.id.typeTask, textTask)
+          transaction.commit()
 
-    }
+      }
 
-    /*выбор повторения задачи*/
-    @OnClick(R.id.textChoose)
-    fun onClickChooseReplay() {
+      /*выбор повторения задачи*/
+      @OnClick(R.id.textChoose)
+      fun onClickChooseReplay() {
 
 
-        onSaveInstanceState(outState)
+          onSaveInstanceState(outState)
 
-        val replay = Replay()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+          val replay = Replay()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.replace(R.id.linerLayout, replay)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
+          transaction.replace(R.id.linerLayout, replay)
+          transaction.addToBackStack(null)
+          transaction.commit()
+      }
 
-    /*увелечение фотографии*/
-    fun photoZoom() {
-        val photoIncrease: Fragment = PhotoIncrease()
-        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+      /*увелечение фотографии*/
+      fun photoZoom() {
+          val photoIncrease: Fragment = PhotoIncrease()
+          val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
-        transaction.replace(R.id.relativeLayout, photoIncrease)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
+          transaction.replace(R.id.relativeLayout, photoIncrease)
+          transaction.addToBackStack(null)
+          transaction.commit()
+      }
 
-    /*Добавить подзадачу*/
-    @OnClick(R.id.btnAddSubTask)
+      /*Добавить подзадачу*/
+      @OnClick(R.id.btnAddSubTask)
+      fun onClickAddSubTask() {
+
+          val listViewSubTask = view!!.findViewById<ListView>(R.id.listSubTask)
+          val subTask = SubTasks()
+          val listSubTask = subTask.createSubTask(listViewSubTask)
+
+          val adapter = ArrayAdapter<String>(
+              activity!!.applicationContext,
+              android.R.layout.simple_list_item_1,
+              listSubTask
+          )
+          listViewSubTask.adapter = adapter
+      }
+
+      /*получаем данные введенные пользователем в editText и передаем в активити WindowTask*/
+      @OnClick(R.id.add)
+      fun onclickAdd() {
+
+          inizializationEdit()
+
+          val dataBaseTask = DataBaseTask()
+          dataBaseTask.createTask(textTask!!)
+
+          if (arguments != null) {
+              val value = arguments!!.getString("Period")
+          }
+
+          val intent = Intent(activity!!.applicationContext, MainWindowCasePlanning()::class.java)
+          intent.putExtra("nameTask", textTask)
+          startActivity(intent)
+      }
+
+
+      private fun inizializationEdit() {
+          editTextTaskName = activity!!.findViewById<EditText>(R.id.taskText)
+          }
+   */
+
+    /*динамическое добавление подзадач*/
+    @OnClick(R.id.addSubTasks)
     fun onClickAddSubTask() {
 
-        val listViewSubTask = view!!.findViewById<ListView>(R.id.listSubTask)
-        val subTask = SubTasks()
-        val listSubTask = subTask.createSubTask(listViewSubTask)
+        val layoutInflater = layoutInflater
 
-        val adapter = ArrayAdapter<String>(
-            activity!!.applicationContext,
-            android.R.layout.simple_list_item_1,
-            listSubTask
-        )
-        listViewSubTask.adapter = adapter
-    }
-
-    /*получаем данные введенные пользователем в editText и передаем в активити WindowTask*/
-    @OnClick(R.id.add)
-    fun onclickAdd() {
-
-        inizializationEdit()
-
-        val dataBaseTask = DataBaseTask()
-        dataBaseTask.createTask(textTask!!)
-
-        if (arguments != null) {
-            val value = arguments!!.getString("Period")
-        }
-
-        val intent = Intent(activity!!.applicationContext, MainWindowCasePlanning()::class.java)
-        intent.putExtra("nameTask", textTask)
-        startActivity(intent)
-    }
-
-
-    private fun inizializationEdit() {
-        editTextTaskName = activity!!.findViewById<EditText>(R.id.taskText)
-        }
- */
-
-    @OnClick(R.id.addSubTasks)
-    fun onClickAddSubTask(){
-        val t = EditText(activity!!.applicationContext)
         val linerLayoutSubTask = view!!.findViewById<LinearLayout>(R.id.editSubTask)
-        t.layoutParams =
-            LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        linerLayoutSubTask.addView(t)
 
+        val viewFr = layoutInflater.inflate(R.layout.add_sub_tasks, null, false)
+
+        val relativeLayoutSubTasks = viewFr.findViewById<RelativeLayout>(R.id.rel)
+
+
+        relativeLayoutSubTasks.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        if (relativeLayoutSubTasks.parent != null) {
+            (relativeLayoutSubTasks.parent as ViewGroup).removeView(relativeLayoutSubTasks)
+        }
+
+        linerLayoutSubTask.addView(relativeLayoutSubTasks)
+
+        val btnDeleted = viewFr.findViewById<ImageButton>(R.id.btnDeleted)
+        btnDeleted.setOnClickListener { onClickDeletedSubTask(relativeLayoutSubTasks) }
+
+        val btnOkSubTasks = viewFr.findViewById<ImageButton>(R.id.btnOkCreate)
+        btnOkSubTasks.setOnClickListener { onClickCreateSubTask(viewFr, btnDeleted, btnOkSubTasks) }
+    }
+
+    /*удаляет подзадачу*/
+    fun onClickDeletedSubTask(relativeLayoutSubTasks : RelativeLayout){
+
+        relativeLayoutSubTasks.visibility = RelativeLayout.GONE
+    }
+
+    /*создает подзадачу*/
+    fun onClickCreateSubTask(viewFr : View,
+                             btnDeleted : ImageButton,
+                             btnOkSubTasks : ImageButton )
+    {
+
+        val editTextSubTasks = viewFr.findViewById<EditText>(R.id.editTextSubTasks)
+        editTextSubTasks.isEnabled = false
+        btnDeleted.visibility = ImageButton.GONE
+        btnOkSubTasks.visibility = ImageButton.GONE
+
+    }
 
 }
-
-    }
