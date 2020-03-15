fac63100094da55c6edd6da98fb1e396337f65af
Julia <negodyaeva.yulya@mail.ru>
2020-03-16 01:05:16 +0800

    сделан переворот главной страницы

--- CasePlanning/app/src/main/java/com/example/caseplanning/MainWindowCasePlanning.kt
+++ CasePlanning/app/src/main/java/com/example/caseplanning/MainWindowCasePlanning.kt
@@ -1,18 +1,40 @@
 package com.example.caseplanning
 
 
+import android.annotation.SuppressLint
+import android.app.Fragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 
 @Suppress("DEPRECATION")
 class MainWindowCasePlanning : AppCompatActivity() {
 
+    private var fragmentTransaction : FragmentTransaction?  = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_window_case_planning)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.linerLayout, WindowTask())
-            .commit()
+
+    }
+
+    override fun onPostResume() {
+        if (supportFragmentManager.findFragmentById(R.id.linerLayout) != null)
+        {
+            supportFragmentManager
+                .beginTransaction()
+                .remove(
+                    supportFragmentManager
+                        .findFragmentById(R.id.linerLayout)!!
+                )
+                .commit()
+        }
+            supportFragmentManager.beginTransaction()
+                .add(R.id.linerLayout, WindowTask())
+                .commitAllowingStateLoss()
+
+        super.onPostResume()
     }
 }
